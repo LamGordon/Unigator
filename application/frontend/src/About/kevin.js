33829121c8ac97ddc3454b952636f2ad16fd24e4
Kevin Huang <khuang2@mail.sfsu.edu>
2020-02-27 16:57:04 -0800

    updated kevin about

--- application/frontend/src/About/kevin.js
+++ application/frontend/src/About/kevin.js
@@ -14,7 +14,13 @@ const Kevin = () => {
                 <h3>Frontend engineer</h3>
                 <h3>khuang2@mail.sfsu.edu</h3>
             </div>
-            <div className='desc'>My name is Kevin Huang. I am a computer science major at San Francisco State University.</div>
+            <div className='desc'>
+                <ul>
+                    <li>My name is Kevin Huang.</li>
+                    <li>I am a computer science major at San Francisco State University.</li>
+					<li>My favorite food are cheeseburgers.</li>
+                </ul>
+            </div>
         </div>
     );
 };
