13669a7a505bb3fb89a055b8c76524e144e43e02
Jorge Landaverde <jorge100394@gmail.com>
2020-04-04 18:09:16 -0700

    Revert "prototype of home page" removing direct  push to develop branch becqause it breaks project
    This reverts commit 82198980f061064c58adfe4cdc5562bd568bcb9a.

--- application/frontend/src/Home/home.js
+++ application/frontend/src/Home/home.js
@@ -4,48 +4,9 @@ const Home = () => {
     return (
         <div style={{ paddingTop: "40px" }}>
             <h1>UniGator</h1>
-
-            <div className="content">
-                <div className="container">
-                    <section className="section">
-                        <ul>
-                            {this.state.list.map(item => (
-                                <li key={item}>{item}</li>
-                            ))}
-                        </ul>
-                    </section>
-                    <hr />
-                    <section className="section">
-                        <form className="form" id="addItemForm">
-                            <input
-                                type="text"
-                                className="input"
-                                id="addInput"
-                                placeholder="Something that needs ot be done..."
-                            />
-                            <button className="button is-info" onClick={this.addItem}>
-                                Add Event
-                             </button>
-                            <div>
-                                <button>
-                                    Show Events
-                                </button>
-
-                                <div className="menu">
-                                    <button> Schedule an event </button>
-                                    <button> Sign up for event </button>
-                                    <button> see events saved </button>
-                                </div>
-                            </div>
-
-                        </form>
-                    </section>
-                </div>
-
-                <br></br>
-                <h3>Connecting, .... </h3>
-            </div>
-            </div>
+            <br></br>
+            <h3>Connecting, .... </h3>
+        </div>
     );
 };
 
