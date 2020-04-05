22b6eeef7921567aa41394606f6f49a87e2b35fa
Lionel Wong <lionelwong60@yahoo.com>
2020-04-04 19:55:45 -0700

    updates

--- application/frontend/src/Home/home.js
+++ application/frontend/src/Home/home.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './home.css';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+
 
 const Home = () => {
+
+    const [event, setEvent] = React.useState('');
+    const [category, setCategory] = React.useState('Categories');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        if (category != "Categories") {
+            axios.post(`/events`, {name: event})
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                })
+        }
+    }
+
+    const onClickHandler = category =>{
+        const value = category.target.getAttribute('value');
+        setCategory(value);
+
+    }
+
     return (
         <div >
             <h1 >UniGator</h1>
             <h3 className='button'>Log in</h3>
             <h3 className='button2'>Sign Up</h3>
             <div >
-                <input className='border1'>
-                </input>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Search:
+                        <input className='border1' type="text" name="name" onChange={e => setEvent(e.target.value)} />
+                    </label>
+                    <Button className='right' variant="dark">Submit</Button>{' '}
+                </form>
 
-                <button className='right'>Submit</button>
             </div>
             <div>
                 <Dropdown>
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Categories
+                        {category}
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <Dropdown.Item value='Categories' onClick={onClickHandler}>None</Dropdown.Item>
+                        <Dropdown.Item value='Technology' onClick={onClickHandler}>Technology</Dropdown.Item>
+                        <Dropdown.Item value='Education' onClick={onClickHandler}>Education</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
             </div>
         </div>
-
     );
 };
 
