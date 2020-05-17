585731b4e110824b6e93c4754090ad450f35298b
Kevin Huang <khuang2@mail.sfsu.edu>
2020-05-16 23:39:55 -0700

    wip point shop

--- application/frontend/src/pages/Store/Store.js
+++ application/frontend/src/pages/Store/Store.js
@@ -11,83 +11,67 @@ import {
     Carousel,
     Jumbotron,
 } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+const RenderPointShop = styled.div`
+	background-color: #f8f8f8;
+
+	justify-content: space-evenly;
+`;
 
 class Store extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pointshop: [],
+        };
+        this.getPointsShop = this.getPointsShop.bind(this);
+    }
+
+    getPointsShop(){    //currently returns nothing
+        const { pointshop } = this.state;
+
+        const config = {
+            headers: { 'Content-Type': 'text/plain' }
+        }
+
+        axios
+            .get(`/pointshop`, config)
+            .then((res) => {
+                this.setState({ pointshop: res.data });
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     render() {
+        const { pointshop } = this.state;
+        this.getPointsShop();
         return (
-            <Container>
-                <h1 style={{textAlign: 'center'}}>Unigator Store</h1>
-                <p>Your Points: 1300</p>
-                <h4>Text Fonts - Change the font style on your profile</h4>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Lucida Console'}}>Lucida Console</Card.Title>
-                                <p>Price: 200 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Impact'}}>Impact</Card.Title>
-                                <p>Price: 300 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Comic Sans MS'}}>Comic Sans MS</Card.Title>
-                                <p>Price: 400 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Courier New'}}>Courier New</Card.Title>
-                                <p>Price: 200 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Arial Black'}}>Arial Black</Card.Title>
-                                <p>Price: 300 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="" />
-                            <Card.Body>
-                                <Card.Title style={{fontFamily: 'Georgia'}}>Georgia</Card.Title>
-                                <p>Price: 400 Points</p>
-                                <Button variant="primary">Purchase</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+            <RenderPointShop>
+                <Container>
+                    <Row className="justify-content-md-center">
+                        {pointshop.map((item) => (
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img
+                                    variant="top"
+                                    src="https://d3vhc53cl8e8km.cloudfront.net/hello-staging/wp-content/uploads/2017/12/22223742/Events-1200x630.jpg"
+                                />
+                                <Card.Body>
+                                    <Card.Title>{item.name}</Card.Title>
+                                    <Button>Purchase</Button>
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </Row>
+                </Container>
+            </RenderPointShop>
         );
     }
 }
 
 export default Store;
-
